961686b323f65354b20cf7ac82830f5f867d383b
seong2808 <seong2808@naver.com>
2023-03-06 22:13:13 +0900

    second commit

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1);
-
-
 function handleh1Click() {
-   h1.style.color = "blue";
+    const clickedClass = "clicked";
+     if (h1.className === clickedClass) {
+        h1.className = "";
+     } else {
+        h1.className = clickedClass;
+     }
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!"
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier");
-}
-
-function handleWindowPaste() {
-    alert("Paste");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
-}
 
 h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
