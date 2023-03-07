bb1ae669c5181fbd046dd2dcd234330d8b842c09
seong2808 <seong2808@naver.com>
2023-03-07 15:31:10 +0900

    last commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleh1Click() {
-    const clickedClass = "clicked";
-     if (h1.className === clickedClass) {
-        h1.className = "";
-     } else {
-        h1.className = clickedClass;
-     }
+   h1.classList.toggle("clicked");
+
+   // const clickedClass = "clicked";
+   //   if (h1.classList.contains(clickedClass)) {
+   //      h1.classList.remove(clickedClass);
+   //   } else {
+   //      h1.classList.add(clickedClass);
+   //   }
 }
 
 
